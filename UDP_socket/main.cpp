a82e810b4c43c38b466e4494d29eaa8253e5e966
Yiğit Karaağaç <71917785+Sqv51@users.noreply.github.com>
2022-06-17 10:45:31 +0300

    Barebone udp socket yapıldı

---
+++ UDP_socket/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include "myudp.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyUDP Server;
+    MyUDP client;
+
+    client.SayHello();
+    return a.exec();
+}
